ad4951df90262d7a422d0363d0bdccf05eaa96da
Henrick <ricknogueira1231@gmail.com>
2025-01-30 14:52:24 -0300

    2.9. Desabilitando testes unitários

--- src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
@@ -1,6 +1,7 @@
 package com.algaworks.junit.utilidade;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
@@ -8,6 +9,7 @@ import java.time.Duration;
 class SimuladorEsperaTest {
 
     @Test
+    @Disabled("Não é mais aplicavel")
     void deveEsperarENaoDarTimeout() {
         Assertions.assertTimeoutPreemptively(Duration.ofSeconds(1), () -> SimuladorEspera.esperar(Duration.ofMillis(10)));
     }
