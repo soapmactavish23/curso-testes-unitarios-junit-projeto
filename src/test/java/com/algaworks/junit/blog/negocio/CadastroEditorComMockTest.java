d52af27f00c90a59c20725b6fa08282a3320b5bb
Henrick <ricknogueira1231@gmail.com>
2025-02-03 13:52:59 -0300

    4.7. Verificando chamada de métodos com mock usando Mockito verify

--- src/test/java/com/algaworks/junit/blog/negocio/CadastroEditorComMockTest.java
+++ src/test/java/com/algaworks/junit/blog/negocio/CadastroEditorComMockTest.java
@@ -6,34 +6,53 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@ExtendWith(MockitoExtension.class)
+@DisplayNameGeneration(DisplayNameGenerator.class)
 public class CadastroEditorComMockTest {
 
-    CadastroEditor cadastroEditor;
     Editor editor;
+
+    @Mock
+    ArmazenamentoEditor armazenamentoEditor;
+
+    @Mock
+    GerenciadorEnvioEmail gerenciadorEnvioEmail;
+
+    @InjectMocks
+    CadastroEditor cadastroEditor;
 
     @BeforeEach
     void init() {
         editor = new Editor(null, "Alex", "alex@email.com", BigDecimal.TEN, true);
 
-        ArmazenamentoEditor armazenamentoEditor = Mockito.mock(ArmazenamentoEditor.class);
-        Mockito.when(armazenamentoEditor.salvar(editor))
-                .thenReturn(new Editor(1L, "Alex", "alex@email.com", BigDecimal.TEN, true));
-        GerenciadorEnvioEmail gerenciadorEnvioEmail = Mockito.mock(GerenciadorEnvioEmail.class);
-
-        cadastroEditor = new CadastroEditor(armazenamentoEditor, gerenciadorEnvioEmail);
+        Mockito.when(armazenamentoEditor.salvar(Mockito.any(Editor.class)))
+                .thenAnswer(invocation -> {
+                    Editor editoPassado = invocation.getArgument(0, Editor.class);
+                    editoPassado.setId(1L);
+                    return editoPassado;
+                });
     }
 
     @Test
     void Dado_um_editor_valido_Quando_criar_Entao_deve_retornar_um_id_de_cadastro() {
         Editor editorSalvo = cadastroEditor.criar(editor);
         assertEquals(1L, editorSalvo.getId());
+    }
+
+    @Test
+    void Dado_um_editor_valido_quando_criar_Entao_deve_chamar_metodo_salvar_do_armazenamento() {
+        cadastroEditor.criar(editor);
+        Mockito.verify(armazenamentoEditor, Mockito.times(1)).salvar(Mockito.any(Editor.class));
     }
 
 
